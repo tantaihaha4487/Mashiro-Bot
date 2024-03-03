bf9f4c4ca012ec6c6379c86e104c6fc58c5938cc
TantaiHaha <tantaiiottwo@gmail.com>
2024-03-03 14:49:22 +0700

    DM notify when bot online

---
+++ src/events/sendDM.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder } = require("discord.js");
+require('dotenv').config();
+
+
+const { ADMIN_USER_ID } = process.env;
+
+
+module.exports = {
+	name: 'ready',
+	once: true,
+	execute(client) {
+
+        // Send DM notification when bot online.
+        client.users.fetch(ADMIN_USER_ID)
+            .then(user => {
+
+                embed = new EmbedBuilder()
+                    .setColor(0xf9c6cf)
+                    .setTitle(`Bot Online!!`)
+                    .setDescription(`🟢 ${client.user.username} was online.`)
+                    .setAuthor({ name: `${client.user.username}`, iconURL: client.user.displayAvatarURL() })
+                    .setTimestamp();
+
+                user.send({ embeds: [embed] });
+                
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+	},
+};
